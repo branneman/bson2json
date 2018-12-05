6d420b0e4c8f08481b1214f19d4491c8d0f13795
Bran van der Meer <bran.van.der.meer@protonmail.com>
2018-12-05 16:38:53 +0100

    feat: Implemented bson upload file functionality

--- static/app.js
+++ static/app.js
@@ -1,21 +1,41 @@
 var bsonInputElement = document.querySelector('#bson_input')
+var bsonInputButtonElement = document.querySelector('#bson_input-button')
+var bsonInputFilenameElement = document.querySelector('#bson_input-filename')
 var bsonHexInputElement = document.querySelector('#bson_hexinput')
 var errorOutputElement = document.querySelector('.error')
 var bsonOutputElement = document.querySelector('#bson_output')
 var bsonOutputWrapperElement = document.querySelector('.layout__feature--bson')
 var deserializeElement = document.querySelector('#deserialize')
 var deserializeHexElement = document.querySelector('#deserialize-hex')
+var clearElement = document.querySelector('#clear')
 var clearHexElement = document.querySelector('#clear-hex')
 
+bsonInputElement.addEventListener('change', function() {
+  deserialize(bsonInputElement.files)
+})
+
+bsonInputButtonElement.addEventListener('click', function() {
+  bsonInputElement.click()
+})
+
+deserializeElement.addEventListener('click', function() {
+  deserialize(bsonInputElement.files)
+})
+
+clearElement.addEventListener('click', function() {
+  bsonInputElement.value = ''
+  bsonInputFilenameElement.innerHTML = 'none'
+  bsonOutputWrapperElement.classList.add('hidden')
+  errorOutputElement.classList.add('hidden')
+  bsonOutputElement.classList.add('hidden')
+})
+
+bsonHexInputElement.addEventListener('change', function() {
+  deserializeHex(bsonHexInputElement.value)
+})
+
 deserializeHexElement.addEventListener('click', function() {
-  var bsonHexData = bsonHexInputElement.value
-  try {
-    var bsonData = hexStringToUint8Array(bsonHexData)
-    var jsonData = BSON.deserialize(bsonData)
-    showOutput(jsonData)
-  } catch (err) {
-    showError()
-  }
+  deserializeHex(bsonHexInputElement.value)
 })
 
 clearHexElement.addEventListener('click', function() {
@@ -24,6 +44,33 @@ clearHexElement.addEventListener('click', function() {
   errorOutputElement.classList.add('hidden')
   bsonOutputElement.classList.add('hidden')
 })
+
+function deserialize(files) {
+  if (files.length < 1) return
+  var reader = new FileReader()
+  reader.onload = function() {
+    try {
+      var bsonData = new Uint8Array(this.result)
+      var jsonData = BSON.deserialize(bsonData)
+      showOutput(jsonData)
+      bsonInputFilenameElement.innerHTML = files[0].name
+    } catch (err) {
+      showError()
+    }
+  }
+  reader.readAsArrayBuffer(files[0])
+}
+
+function deserializeHex(bsonHexData) {
+  if (!bsonHexData) return
+  try {
+    var bsonData = hexStringToUint8Array(bsonHexData)
+    var jsonData = BSON.deserialize(bsonData)
+    showOutput(jsonData)
+  } catch (err) {
+    showError()
+  }
+}
 
 function showOutput(output) {
   var json = JSON.stringify(output, 0, 2)
